e598e34c52603954d0c5f5d46927d6a75a302f5f
aykutserbest <aykut.serbest@yandex.com>
2022-08-05 01:47:36 +0300

    Input actions and hole joystick control system

--- Assets/_GameCore/Scripts/HoleController.cs
+++ Assets/_GameCore/Scripts/HoleController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.InputSystem;
 
 public class HoleController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PlayerInput _playerInput;
+    private int speed=2 ;
+    
     void Start()
     {
-        
+        _playerInput = gameObject.GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector2 input = _playerInput.actions["Movement"].ReadValue<Vector2>();
+        Vector3 move = new Vector3(-input.x, 0, -input.y);
+        Vector3 newPosition = transform.position + move * Time.deltaTime * speed;
         
+        transform.DOMove(newPosition, Time.deltaTime);
+        
+        
+        
+      //  Vector3 targetPosition = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+       // transform.DOMove(targetPosition, 3f);
     }
 }
