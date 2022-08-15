62e2378220a4f11af3f54318827df0c553ee2a9e
aykutserbest <aykut.serbest@yandex.com>
2022-08-16 02:07:42 +0300

    Discharge Area Controller trigger methods

---
+++ Assets/_GameCore/Scripts/DischargeAreaController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DischargeAreaController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(" Trigger as2daa3s");
+    }
+}
